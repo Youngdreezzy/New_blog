a746e5f5b54a59f726d7f9f58c904b6f1bb982cb
youngdreezzy <afolabiopesimon@gmail.com>
2025-03-06 15:21:51 +0100

    trying to deploy with render

--- New_blog/settings.py
+++ New_blog/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path        
 import os                                       
 from django.contrib.messages import constants as message_constants
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'New_blog.urls'
@@ -68,7 +70,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'blog_App.context_processors.user_profile',
+                
             ],
         },
     },
@@ -81,18 +83,19 @@ WSGI_APPLICATION = 'New_blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": 'fresh_db',
-        'USER': 'root',
-        'PASSWORD': 'new_password',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS':  {
-            'autocommit':True,
-            'charset': 'utf8mb4',
-        }
-    }
+    'default': dj_database_url.config(default="postgresql://fresh_db_user:yLd23GmrOkGbR7ezRIfbujTGo5S4jFSE@dpg-cv4pmannoe9s73ceo92g-a.oregon-postgres.render.com/fresh_db")
+    # "default": {
+    #     "ENGINE": "django.db.backends.mysql",
+    #     "NAME": 'fresh_db',
+    #     'USER': 'root',
+    #     'PASSWORD': 'new_password',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306',
+    #     'OPTIONS':  {
+    #         'autocommit':True,
+    #         'charset': 'utf8mb4',
+    #     }
+    # }
 }
 
 
@@ -136,9 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
